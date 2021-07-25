cf377c6ecd41011df09b9aa62533c57bbce93c36
Edwin Agbenyega <eddybanks03@gmail.com>
2021-07-25 15:43:01 -0700

    Add basic styled components for landing page
    Added header, main and footer styled components

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderBar, HeaderBrand } from "./custom-styles";
+
+const Header = () => {
+  return (
+    <HeaderBar>
+      <HeaderBrand>Drello</HeaderBrand>
+    </HeaderBar>
+  );
+};
+
+export default Header;
