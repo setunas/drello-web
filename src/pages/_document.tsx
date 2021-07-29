e1e5a209a9a9b0ac1295066a193c0a2a13f49ff6
Edwin Agbenyega <eddybanks03@gmail.com>
2021-07-28 20:21:08 -0700

    Switch files from js to ts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,11 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Arapey:ital@0;1&family=Baloo+Chettan+2&family=David+Libre:wght@400;500&family=Fira+Sans:wght@100;400;600&family=Pacifico&display=swap"
             rel="stylesheet"
