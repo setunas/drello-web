e4d51680865c9c1d8240367d83b7c48bb1655d75
nao-noah <naomooo.official@gmail.com>
2021-10-03 21:42:07 -0700

    Add basic UIs

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,3 +1,31 @@
-const SignupPage = () => {};
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+const SignupPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = () => {};
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email", { required: true })} />
+      {errors.email && <span>This field is required</span>}
+      <input {...register("password", { required: true })} />
+      {errors.password && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default SignupPage;
