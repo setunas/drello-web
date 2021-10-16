9908806b637782badd789901b72781f944b90d08
nao-noah <naomooo.official@gmail.com>
2021-10-15 21:46:03 -0700

    Add urls for drello-api

---
+++ src/utils/url/drello-api.ts
@@ -0,0 +1,19 @@
+import { serverEnv } from "src/constants/system";
+import { currentEnv } from "src/utils/server-env";
+
+export const domain = () => {
+  switch (currentEnv()) {
+    case serverEnv.development:
+    case serverEnv.test:
+      return "http://localhost:3000";
+    case serverEnv.staging:
+    case serverEnv.production:
+    default:
+      throw Error(`Invalid server env: ${currentEnv()}`);
+  }
+};
+
+export const path = {
+  signUp: (signUpId?: number) =>
+    signUpId ? `/api/sign-up/${signUpId}` : `/api/sign-up`,
+};
