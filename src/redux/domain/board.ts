6cd688f17ed415b69b5472c9515e0ebd32abc4f5
nao-noah <naomooo.official@gmail.com>
2021-10-15 22:38:06 -0700

    Add a example code of Redux toolkit for Boards so other members can start easily

---
+++ src/redux/domain/board.ts
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getBoards } from "src/api/drello-api/board";
+import { Board as innerBoard } from "src/types/inner/board.g";
+import { Board as OuterBoard } from "src/types/outer/drello-api/board";
+import { RootState } from "src/redux/root";
+
+interface BoardState {
+  boards: innerBoard[];
+}
+
+const initialState: BoardState = {
+  boards: [],
+};
+
+const convertBoardToInnerType = (ob: OuterBoard): innerBoard => {
+  return {
+    id: ob.id,
+    title: ob.title,
+  };
+};
+
+/**
+ * getBoardsThunk call the function to hit the API to fetch Board list data.
+ */
+export const getBoardsThunk = createAsyncThunk(
+  "board/getBoardsThunk",
+  async () => {
+    const res = await getBoards();
+    return res.data.boards.map((b) => convertBoardToInnerType(b));
+  }
+);
+
+export const slice = createSlice({
+  name: "board",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getBoardsThunk.fulfilled, (state, action) => {
+      state.boards = action.payload;
+    });
+    builder.addCase(getBoardsThunk.rejected, (state, action) => {
+      // handle errors if needed.
+    });
+  },
+});
+
+export const selectBoards = (state: RootState) => state.boardState.boards;
+
+export const boardReducer = slice.reducer;
