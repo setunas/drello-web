b3ae447f0531dfc9f2dfcb1c0eb317a61ae67851
nao-noah <naomooo.official@gmail.com>
2021-10-18 19:48:36 -0700

    Create a mockup of google auth

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 type FormFields = {
   email: string;
@@ -10,11 +11,32 @@ const SignupPage = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<FormFields>();
 
-  const onSubmit = () => {};
+  const onSubmit = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
