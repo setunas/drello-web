3d3db87addba65a11bf6fb6a8831ae4719ee93a9
Edwin Agbenyega <eddybanks03@gmail.com>
2021-10-21 23:50:55 -0700

    Make the header a bit more responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,60 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import {
-  HeaderBar,
-  HeaderBrand,
-  HeaderContainer,
-  LeftNavItems,
-} from "./shared-styles";
+import styled from "styled-components";
 import { path } from "../utils/url/drello-web";
+import { drelloColors } from "./shared-styles";
+
+const HeaderContainer = styled.header`
+  display: grid;
+  padding: 2em 2em 0 2em;
+  @media screen and (min-width: 720px) {
+    padding: 5em 10em 0 10em;
+  }
+`;
+
+const HeaderBar = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 1.5em;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  border-radius: 1em;
+  box-shadow: 0.2em 0.2em 0.3em ${drelloColors.black(0.4)};
+  @media screen and (min-width: 720px) {
+    padding: 2em 5em;
+  }
+`;
+
+const HeaderBrand = styled.h3`
+  font-family: "Arapey", serif;
+  font-size: 1.7em;
+  color: #707070;
+  @media screen and (min-width: 720px) {
+    font-size: 2em;
+  }
+`;
+
+const LeftNavItems = styled.div`
+  display: grid;
+  align-content: center;
+`;
+
+const LoginText = styled.span`
+  display: none;
+  @media screen and (min-width: 720px) {
+    display: block;
+  }
+`;
+
+const InlineAnchor = {
+  display: "grid",
+  gridAutoFlow: "column",
+  gap: "1em",
+  alignContent: "center",
+  textDecoration: "none",
+  color: "inherit",
+};
 
 interface HeaderProps {
   title: string;
@@ -23,8 +71,9 @@ export const Header = ({ title }: HeaderProps) => {
         </Link>
         <LeftNavItems>
           <Link href={path.workspaces()}>
-            <a>
-              <FontAwesomeIcon icon="sign-in-alt" /> Login to get started
+            <a style={InlineAnchor}>
+              <FontAwesomeIcon icon="sign-in-alt" />
+              <LoginText>Login to get started</LoginText>
             </a>
           </Link>
         </LeftNavItems>
--- src/components/shared-styles.ts
+++ src/components/shared-styles.ts
@@ -1,33 +1,11 @@
 import styled from "styled-components";
 
 export const drelloColors = {
-  black: (opacity: number) => `rgba(0,0,0,${opacity})`,
-  white: (opacity: number) => `rgba(255,255,255,${opacity})`,
+  black: (opacity: number = 1) => `rgba(0,0,0,${opacity})`,
+  white: (opacity: number = 1) => `rgba(255,255,255,${opacity})`,
+  greyish: (opacity: number = 1) => `rgba(141, 153, 174,${opacity})`,
 };
 
-// Header styling
-export const HeaderContainer = styled.header`
-  padding: 2em 5em;
-`;
-export const HeaderBar = styled.div`
-  display: grid;
-  grid-template-columns: 1fr auto;
-  padding: 2em 4em;
-  border: 1px solid rgba(0, 0, 0, 0.4);
-  border-radius: 1em;
-  box-shadow: 0.1em 0.1em 0.1em 0.1em rgba(0, 0, 0, 0.5);
-`;
-
-export const HeaderBrand = styled.h3`
-  font-family: "Arapey", serif;
-  font-size: 1.7em;
-  color: #707070;
-`;
-
-export const LeftNavItems = styled.div`
-  display: grid;
-  align-content: center;
-`;
 // Main body
 export const Main = styled.main`
   text-align: center;
