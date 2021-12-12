71b188c626243454f096f7d849e179f215bc52fd
nao-noah <naomooo.official@gmail.com>
2021-12-12 14:41:04 -0800

    Add authState to rootReducer

--- src/redux/root.ts
+++ src/redux/root.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { authReducer } from "src/redux/auth.slice";
 import { boardReducer } from "src/redux/board.slice";
 import { columnReducer } from "src/redux/column.slice";
 import { cardReducer } from "src/redux/card.slice";
 
 export const rootReducer = combineReducers({
+  authState: authReducer,
   boardState: boardReducer,
   columnState: columnReducer,
   cardState: cardReducer,
