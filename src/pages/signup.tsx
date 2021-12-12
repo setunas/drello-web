5a5814046314137891c60984c6d19c04d322a68c
nao-noah <naomooo.official@gmail.com>
2021-12-12 14:39:45 -0800

    Migrate auth code to thunk

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signUp } from "src/redux/auth.slice";
 
 type FormFields = {
   email: string;
@@ -8,6 +9,7 @@ type FormFields = {
 };
 
 const SignupPage = () => {
+  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
@@ -15,27 +17,7 @@ const SignupPage = () => {
   } = useForm<FormFields>();
 
   const handleClick = () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // ...
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
+    dispatch(signUp());
   };
 
   return <button onClick={handleClick}>Sign up with Google</button>;
