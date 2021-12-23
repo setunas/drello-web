4ffd80a1a2c872fb9405379b5a7df398f1a56940
nao-noah <naomooo.official@gmail.com>
2021-12-22 22:07:35 -0800

    Add redirect to board page if currentUser exists

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { signin } from "src/redux/auth.slice";
+import { selectCurrentUser } from "src/redux/user.slice";
+import { path } from "src/utils/url/drello-api";
 
 const SigninPage = () => {
   const dispatch = useDispatch();
+
+  const currentUser = useSelector(selectCurrentUser());
+
+  useEffect(() => {
+    if (currentUser) {
+      window.location.href = path.boards(currentUser.boardId);
+    }
+  }, [currentUser]);
 
   const handleClick = () => {
     dispatch(signin());
--- src/redux/user.slice.ts
+++ src/redux/user.slice.ts
@@ -1,12 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "src/redux/root";
 import { signin } from "src/redux/auth.slice";
-import { getUser, postUser } from "src/api/drello-api/user";
+import { getUser, postUser, User } from "src/api/drello-api/user";
 import { getBoard, postBoard } from "src/api/drello-api/board";
 import { path } from "src/utils/url/drello-api";
 
 interface UserState {
-  currentUser: object | null;
+  currentUser: User | null;
 }
 
 const initialState: UserState = {
