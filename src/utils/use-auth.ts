f7c447f5c8349ce3afd3c5a7ec627281c2174203
nao-noah <naomooo.official@gmail.com>
2021-12-23 20:14:27 -0800

    Add resetCurrentUser when the user is signed out

--- src/utils/use-auth.ts
+++ src/utils/use-auth.ts
@@ -3,7 +3,10 @@ import { useDispatch } from "react-redux";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { updateAuthedUser, resetAuth } from "src/redux/auth.slice";
 import { path } from "src/utils/url/drello-web";
-import { getCurrentUserByIdToken } from "src/redux/user.slice";
+import {
+  getCurrentUserByIdToken,
+  resetCurrentUser,
+} from "src/redux/user.slice";
 import { User } from "src/api/drello-api/user";
 import { AppThunkDispatch } from "src/redux/store";
 
@@ -24,6 +27,7 @@ export const useAuth = () => {
       } else {
         // User is signed out
         dispatch(resetAuth());
+        dispatch(resetCurrentUser());
         if (
           typeof window !== "undefined" && // Client-side-only code
           window.location.pathname !== path.signin() // Unless you are already at signin page
