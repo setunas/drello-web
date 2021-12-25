f3a4968b945dc9689a794550267e0821ad6e2696
nao-noah <naomooo.official@gmail.com>
2021-12-24 22:29:10 -0800

    Move boardtype

--- src/features/board/board.slice.ts
+++ src/features/board/board.slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getBoard } from "src/features/board/board.api";
-import { Board as innerBoard } from "src/types/board.g";
+import { Board as innerBoard } from "src/features/board/board.g";
 import { Board as OuterBoard } from "src/features/board/board.api";
 import { RootState } from "src/utils/redux/root";
 
