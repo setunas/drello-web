8811d7579dc87744086e77150fa8c2ba0c7427ea
Edwin Agbenyega <eddybanks03@gmail.com>
2022-01-08 13:13:28 -0800

    Add Emoji ui component for adding emojis onto webpage

---
+++ src/features/ui/Emoji.tsx
@@ -0,0 +1,15 @@
+interface EmojiProps {
+  label: string;
+  symbol: string;
+}
+
+export const Emoji = ({ label, symbol }: EmojiProps) => (
+  <span
+    className="emoji"
+    role="img"
+    aria-label={label ? label : ""}
+    aria-hidden={label ? "false" : "true"}
+  >
+    {symbol}
+  </span>
+);
