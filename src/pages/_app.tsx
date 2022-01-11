9b9a81baed43b8da57aef47efe2069e59a1c3233
nao-noah <naomooo.official@gmail.com>
2022-01-10 18:53:33 -0800

    Remove unnecessary global icons import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,9 @@
 import { Provider } from "react-redux";
 import { store } from "src/utils/redux/store";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import {
-  fas,
-  faHome,
-  faPlus,
-  faSignInAlt,
-  faTimes,
-  faStar,
-  faCog,
-} from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { initFirebase } from "src/utils/firbase";
 import { GlobalStyle } from "src/features/global-style";
 
-library.add(fab, fas, far, faHome, faPlus, faSignInAlt, faTimes, faStar, faCog);
 initFirebase();
 
 export default function App({ Component, pageProps }: AppProps) {
