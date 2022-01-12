bd2b15f706c4cf7ff2c0b5074b90e1c80deef349
nao-noah <naomooo.official@gmail.com>
2022-01-11 22:44:38 -0800

    Refine go-to button

--- src/features/header/go-to-board-button.tsx
+++ src/features/header/go-to-board-button.tsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 import { path } from "src/utils/url/drello-web";
 import { FC } from "react";
 import styled from "styled-components";
+import { colors } from "src/utils/styles";
 
 const InnerLink = styled.span`
-  color: inherit; /* To change the color of the visited link in Safari*/
+  border-radius: 3em;
+  border: 1px solid ${colors.primary};
+  padding: 0.5em 2em;
+  color: ${colors.primary}; /* To change the color of the visited link in Safari*/
+  font-weight: bold;
   cursor: pointer;
 `;
 
