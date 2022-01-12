a0d6e565fabb66744fd1decb47a1929247fb9608
nao-noah <naomooo.official@gmail.com>
2022-01-11 23:04:54 -0800

    Add signout button

---
+++ src/features/header/signout-button.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import Router from "next/router";
+import { useDispatch } from "react-redux";
+import { colors } from "src/utils/styles";
+import { path } from "src/utils/url/drello-web";
+import { signout } from "src/features/auth/auth.slice";
+
+const SignoutButtonWrapper = styled.div`
+  padding: 0.5em 2em;
+  color: ${colors.text};
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+export const SignoutButton = () => {
+  const dispatch = useDispatch();
+
+  const handleSignout = async () => {
+    await Router.push(path.home());
+    dispatch(signout());
+  };
+
+  return (
+    <SignoutButtonWrapper onClick={handleSignout}>Logout</SignoutButtonWrapper>
+  );
+};
