6caff37df6aee0d4422b94f497d2b91ab26562ef
nao-noah <naomooo.official@gmail.com>
2022-01-11 21:53:30 -0800

    Use new color

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,4 +1,9 @@
 export const colors = {
+  primary: "#2d9f83",
+  secondary: "#F8F32B",
+  third: "",
+  key: "",
+
   black: (opacity: number = 1) => `rgba(0, 0, 0, ${opacity})`,
   white: (opacity: number = 1) => `rgba(255, 255, 255, ${opacity})`,
   greyish: (opacity: number = 1) => `rgba(141, 153, 174, ${opacity})`,
