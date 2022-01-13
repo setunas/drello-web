636498f00dc4ee5547f23e007749e23fd1dbd7d7
nao-noah <naomooo.official@gmail.com>
2022-01-12 18:50:11 -0800

    Modify card design

--- src/features/card/card.tsx
+++ src/features/card/card.tsx
@@ -4,21 +4,19 @@ import { Card as CardType } from "./card.g";
 import { deleteCardThunk } from "./card.slice";
 import { useDispatch } from "react-redux";
 import { DeleteXButton } from "../delete-x-button";
+import { colors } from "src/utils/styles";
 
 const CardMain = styled.div`
+  /* border: 1px solid #ddd; */
+  box-shadow: 0 0 1rem #ddd;
+  border-radius: 0.5rem;
+  padding: 1em;
+  width: 18rem;
   display: grid;
   grid-template-columns: calc(100% - 20px) 10px;
   justify-content: space-between;
-  padding: 1em;
   font-size: 0.9rem;
-  background-color: rgba(255, 255, 255, 0.7);
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  border-radius: 0.2rem;
-`;
-
-const DeleteButton = styled.div`
-  font-weight: bold;
-  cursor: pointer;
+  background-color: ${colors.background}};
 `;
 
 interface CardProps {
