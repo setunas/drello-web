6710231673fa2fbd4cac1b5a5e00325b74cc64a9
nao-noah <naomooo.official@gmail.com>
2022-01-20 22:04:01 -0800

    Add style when deleting a card

--- src/features/card/card.tsx
+++ src/features/card/card.tsx
@@ -5,9 +5,10 @@ import { deleteCardThunk } from "./card.slice";
 import { useDispatch } from "react-redux";
 import { DeleteXButton } from "../common-button/delete-x-button";
 import { colors } from "src/utils/styles";
+import { AppThunkDispatch } from "src/utils/redux/store";
+import { useRef, useState } from "react";
 
-const CardMain = styled.div`
-  /* border: 1px solid #ddd; */
+const CardMain = styled.div<{ isDeleting: boolean }>`
   box-shadow: 0 0 1rem #ddd;
   border-radius: 0.5rem;
   padding: 1.1em;
@@ -20,7 +21,8 @@ const CardMain = styled.div`
 
   font-size: 1rem;
   line-height: 1.3em;
-  background-color: ${colors.backgroundMain}};
+  background-color: ${({ isDeleting }) =>
+    isDeleting ? colors.backgroundSub : colors.backgroundMain}};
 `;
 
 interface CardProps {
@@ -29,16 +31,21 @@ interface CardProps {
 }
 
 export const Card = ({ index, card }: CardProps) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppThunkDispatch>();
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const handleDelete = () => {
-    dispatch(deleteCardThunk({ id: card.id, columnId: card.columnId }));
+    setIsDeleting(true);
+    dispatch(deleteCardThunk({ id: card.id, columnId: card.columnId }))
+      .unwrap()
+      .finally(() => setIsDeleting(false));
   };
 
   return (
     <Draggable draggableId={card.id.toString()} index={index}>
       {(provided) => (
         <CardMain
+          isDeleting={isDeleting}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
